9a0d6c0a3875d17b7fe3345f7f8ec9a76b4f1ca8
Gabriel Lucas Severino <gabriellucasseverino@hotmail.com>
2023-10-03 21:24:51 -0300

    refactor: optimization of the Header componente to accept dynamic properties

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,21 @@
 import { Component } from "react";
 import "./header.css";
+import homeImage from "../../assets/images/home.jpg";
 class Header extends Component {
   render() {
+    const { title, description, image } = this.props;
     return (
       <div className="container">
         <div className="content">
           <div className="text-content">
-            <h1>Pink Cosmeticos</h1>
-            <h2>nossos produtos sao de qualidade</h2>
+            <h1>{title ? title : "Pink Cosmeticos"}</h1>
+            <h2>
+              {description ? description : "nossos produtos sao de qualidade"}
+            </h2>
           </div>
-          <div className="img-content" />
+          <div className="img-content">
+            <img src={image ? image : homeImage} alt="Principal" />
+          </div>
         </div>
       </div>
     );
